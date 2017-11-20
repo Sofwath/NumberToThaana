f55c320d69cc42873da9b1095e29cb0c3ad72464
Sofwath <sofwath@hotmail.com>
2017-11-21 04:08:40 +0800

    Update numtodhivehi.py

--- numtodhivehi.py
+++ numtodhivehi.py
@@ -2,52 +2,31 @@
 #ޮ old code from python 2 migrated to python 3 to be used for part of the dhivehi TTS
 # input must be int 
 
-ehbari = ["ސުމެއް","އެއް","ދޭ","ތިން","ހަތަރު","ފަސް","ހަ","ހަތް","އަށް","ނުވަ","ދިހަ","އެގާރަ","ބާރަ","ތޭރަ","ސާދަ","ފަނަރަ","ސޯޅަ","ސަތާރަ","އަށާރަ","އޮނަވިހި"]
-dhihabari = ["ސުން","ދިހަ","ވިހި","ތިރީސް","ސާޅީސް","ފަންސާސް","ފަސްދޮޅަސް","ހައްދިހަ","އާހި","ނަވައި"]
+ehbari = ["ސުމެއް","އެއް","ދެ","ތިން","ހަތަރު","ފަސް","ހަ","ހަތް","އަށް","ނުވަ","ދިހަ","އެގާރަ","ބާރަ","ތޭރަ","ސާދަ","ފަނަރަ","ސޯޅަ","ސަތާރަ","އަށާރަ","ނަވާރަ","ވިހި","އެކާވީސް","ބާވީސް","ތޭވީސް","ސައުވީސް","ފަންސަވީސް","ސައްބީސް","ހަތާވީސް","އަށާވީސް","ނަވާވީސް"]
+dhihabari = ["ސުން","ދިހަ","ވިހި","ތިރީސް","ސާޅީސް","ފަންސާސް","ފަސްދޮޅަސް","ހައްދިހަ","އައްޑިހަ","ނުވަދިހަ"]
 sunbari = ["","ހާސް","މިލިޔަން","ބިލިޔަން","ޓްރިލިޔަން"]
 
 def Badhalu(inputString):
-    intNum = eval(inputString.strip())
-    if intNum < 1000:
-        return HaasSub(inputString)
-    else:
-        return HaasMathi(inputString)
+	intNum = eval(inputString.strip())
+	if intNum < 1000:
+	    return (HaasSub(inputString))
+	else:
+	    return (HaasMathi(inputString))
 
 def HaasSub(inputNumber):
     number = int(inputNumber)
-    if 0 <= number <= 19:
+    satheyka = "ސަތޭކަ "
+    if (0 <= number <= 29):
         return ehbari[number]
-    elif 20 <= number <= 99:
-        return (dhihabari[int(inputNumber[0])] if inputNumber[-1] == '0'
-                 else dhihabari[int(inputNumber[0])] + ' '
-                + ehbari[int(inputNumber[1])])
-    elif 100 <= number <= 999:
+    elif (30 <= number <= 99):
+        return (dhihabari[int(inputNumber[0])]) if (inputNumber[-1] == "0") else (dhihabari[int(inputNumber[0])] + " " + ehbari[int(inputNumber[1])])
+    elif (100 <= number <= 999):
         rem = number % 100
         dig = number // 100
-        return (ehbari[dig] + "ސަތޭކަ" if rem == 0 else ehbari[dig]
-                + "ސަތޭކަ " + HaasSub(str(rem)))
-
-def constractSun(arrZero):
-    z = 0
-    while z <= arrZero[::-1]:
-    	wrd = HaasSub(str(z)) + " "
-        zap = sunbari[lenArr] + " "
-        if wrd == " ":
-            break
-        elif wrd == "ސުން ":
-            wrd, zap = "", ""
-        resArr.append(wrd + zap)
-        lenArr -= 1
-    yield resArr
-
-def HaasMathi(inputNumber):
-    number = int(inputNumber)
-    arrZero = HaasBuri(number)
-    lenArr = len(arrZero) - 1
-    resArr = []
-    res = "".join(constractSun(resArr)).strip()
-    if res[-1] == ",": res = res[:-1]
-    return res
+        if (dig == 2):
+        	ehbari[2] = "ދުވި"
+        	satheyka = "ސައްތަ "
+        return (ehbari[dig] + satheyka) if (rem == 0) else (ehbari[dig] + satheyka + HaasSub(str(rem)))
 
 def HaasBuri(inputNumber):
     number = int(inputNumber)
@@ -57,5 +36,25 @@ def HaasBuri(inputNumber):
         number //= 1000
     return arrHaas
 
-strNum = input('integer:\n>> ')
-print Badhalu(strNum)
+def HaasMathi(inputNumber):
+    number = int(inputNumber)
+    arrZero = HaasBuri(number)
+    lenArr = len(arrZero) - 1
+    resArr = []
+    z=0
+    for z in arrZero[::-1]:
+        wrd = HaasSub(str(z)) + " "
+        zap = sunbari[lenArr] + " "
+        if wrd == " ":
+            break
+        elif wrd == "ސުން ":
+            wrd, zap = "", ""
+        resArr.append(wrd + zap)
+        lenArr -= 1
+    res = "".join(resArr).strip()
+    if res[-1] == ",": res = res[:-1]
+    return res
+
+
+strNum = input("integer:\n>> ")
+print (Badhalu(strNum))
